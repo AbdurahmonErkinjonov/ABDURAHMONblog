cf57abb6fe5b97ea13ccdd9e9380a802b529867b
Abdurahmon Erkinjonov <abdirahmon31@gmail.com>
2026-03-31 17:39:06 +0500

    first commit

--- dars-1/apps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AuthorAPIView, GenreAPIView, BookAPIView
-urlpatterns = [
-    path('authors/', AuthorAPIView.as_view(), name='author-list'),  
-    path('genres/', GenreAPIView.as_view(), name='genre-list'),
-    path('books/', BookAPIView.as_view(), name='book-list'),
-]
